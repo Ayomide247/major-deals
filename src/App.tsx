7874e1a0eeee9d0eba282af74dc5c19b7be992ec
Ayomide <bayomide247@gmail.com>
2025-05-17 17:37:04 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="bg-primary h-screen min-w-screen flex flex-col items-center justify-center ">
+        Major Deals
+      </div>
+    </>
+  );
+}
+
+export default App;
